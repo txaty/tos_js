9ef7e0089502384c958916956933e7f7213922c8
tommytim0515 <xtianae@connect.ust.hk>
2021-09-09 16:10:32 +0800

    Clean the code

--- Inc/task.h
+++ Inc/task.h
@@ -5,11 +5,6 @@
 // mutex
 extern k_mutex_t mutex_js_engine;
 
-// #define TASK_SIZE_JS_EXEC 1536u
-// extern uint8_t stk_js_exec[TASK_SIZE_JS_EXEC];
-// extern k_task_t k_js_exec;
-// void task_js_exec(void *pdata);
-
 // task 
 // toggle LED
 #define TASK_SIZE_TOGGLE_LED 256u
--- Src/task.c
+++ Src/task.c
@@ -10,24 +10,6 @@
 // mutex
 k_mutex_t mutex_js_engine;
 
-// task
-// k_task_t k_js_exec;
-// uint8_t stk_js_exec[TASK_SIZE_JS_EXEC];
-// void task_js_exec(void *pdata)
-// {
-//     printf("js_exec\n");
-//     HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
-//     k_err_t err = tos_mutex_pend(&mutex_js_engine);
-//     if (err == K_ERR_NONE)
-//     {
-//         JS *js = js_create_static();
-//         js_driver_init(js);
-//         js_eval(js, js_app_led_control, ~0);
-//         tos_mutex_post(&mutex_js_engine);
-//     }
-//     tos_sleep_ms(500);
-// }
-
 k_task_t k_toggle_led;
 uint8_t stk_toggle_led[TASK_SIZE_TOGGLE_LED];
 void task_toggle_led(void *pdata)
